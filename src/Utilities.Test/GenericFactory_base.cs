58d11738370e4b5dc6c7519432410511566ed23b
Mark Visschers <visschers.m@gmail.com>
2025-03-07 00:22:29 +0100

    trial and error

--- src/Utilities.Test/GenericFactory_base.cs
+++ src/Utilities.Test/GenericFactory_base.cs
@@ -91,5 +91,53 @@ namespace Matr.Utilities.Test
             var services = GetOwnServices(container);
             return CreateContainerBuilder(services);
         }
+
+        /// <summary>
+        /// Gets all currently registered services.
+        /// </summary>
+        /// <returns>List of tuples of service types and services.</returns>
+        public List<(Type, object)> GetRegisteredServices()
+        {
+            return GetOwnServices(container).ToList();
+        }
+
+        private (Type ServiceType, object Service)[] GetOwnServices(IContainer container)
+        {
+            return container.ComponentRegistry.Registrations
+                 .SelectMany(x => x.Services)
+                 .Where(x => (x as TypedService) != null)
+                 .Where(x => (x as TypedService).ServiceType != typeof(ILifetimeScope))
+                 .Where(x => (x as TypedService).ServiceType != typeof(IComponentContext))
+                 .Select(x => ((x as TypedService).ServiceType, TryResolve(x)))
+                 .ToArray();
+        }
+
+        private object TryResolve(Service x)
+        {
+            try
+            {
+
+                return container.Resolve((x as TypedService).ServiceType);
+            }
+            catch (DependencyResolutionException)
+            {
+                return null;
+            }
+        }
+
+        // TODO: Should Service not be lowercase here?
+        private static ContainerBuilder CreateContainerBuilder((Type ServiceType, object Service)[] services)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            services.Where(x => x.Service == null)
+                .ToList()
+                .ForEach(x => containerBuilder.RegisterType(x.ServiceType));
+
+            services.Where(x => x.Service != null)
+                      .ToList()
+                      .ForEach(x => containerBuilder.RegisterInstance(x.Service).As(x.ServiceType));
+            return containerBuilder;
+        }
     }
 }
