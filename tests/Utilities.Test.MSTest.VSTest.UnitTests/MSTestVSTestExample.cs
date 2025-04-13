ca22242adbcf19b251bee7870e0b838420b71223
Mark Visschers <visschers.m@gmail.com>
2025-04-13 18:34:29 +0200

    fixes tests

---
+++ tests/Utilities.Test.MSTest.VSTest.UnitTests/MSTestVSTestExample.cs
@@ -0,0 +1,42 @@
+﻿using Matr.Utilities.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Utilities.Test.MSTest.VSTest.UnitTests
+{
+
+    [TestClass]
+    public sealed class Test1 : TestBase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var returnValue = 1;
+
+            var mockedDependency = Substitute.For<ITestDependency>();
+            mockedDependency.Add(Arg.Any<int>(), Arg.Any<int>())
+                .Returns(returnValue);
+
+            factory.RegisterOrReplaceService(mockedDependency);
+            var testClass = factory.Create<TestClass>();
+
+            var result = testClass.DoAddition(1, 1);
+
+            Assert.AreEqual(returnValue, result);
+
+        }
+
+        public class TestClass
+        {
+            private readonly ITestDependency _someDependency;
+
+            public TestClass(ITestDependency someDependency) => _someDependency = someDependency;
+            public int DoAddition(int x, int y) => _someDependency.Add(x, y);
+        }
+
+        public interface ITestDependency
+        {
+            int Add(int x, int y);
+        }
+    }
+}
